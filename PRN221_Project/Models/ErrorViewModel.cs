a963e3988a8be16de5e3b2c4207bc4cb43e0653b
datndw <103067885+datndw@users.noreply.github.com>
2023-03-05 21:59:09 +0700

    Add project files.

---
+++ PRN221_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN221_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
